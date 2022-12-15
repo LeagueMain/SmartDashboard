29c2b29e5417317082e60d783a15186113908d2e
LeagueMain <119674701+LeagueMain@users.noreply.github.com>
2022-12-15 15:58:23 +0000

    added node

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,23 +6,17 @@ apt-get install -y \
   gnupg2 \
   jq \
   sudo \
-  zsh \
-  vim \
-  build-essential \
-  openssl
+  zsh
 
-## Install rustup and common components
-curl https://sh.rustup.rs -sSf | sh -s -- -y 
-rustup install nightly
-rustup component add rustfmt
-rustup component add rustfmt --toolchain nightly
-rustup component add clippy 
-rustup component add clippy --toolchain nightly
+# set-up and install yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt-get update && apt-get install yarn -y
 
-cargo install cargo-expand
-cargo install cargo-edit
+# instsal nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
-## setup and install oh-my-zsh
+# setup and install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp -R /root/.oh-my-zsh /home/$USERNAME
 cp /root/.zshrc /home/$USERNAME
